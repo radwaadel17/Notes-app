c8e01821c441813522099e0b15a92745163cb8fc
radwaadel17 <radwaadel959@gmail.com>
2024-11-18 22:34:17 +0200

    floating action button

--- app/lib/Notes view.dart
+++ app/lib/Notes view.dart
@@ -1,5 +1,6 @@
 import 'package:app/widgets/NotesViewBody.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
@@ -7,9 +8,12 @@ class NoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      /* floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: (){} , child: const Icon(Icons.add) ,
-        ), */
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.r)
+        ),
+        ), 
       body: NotesViewBody(
       ),
     );
