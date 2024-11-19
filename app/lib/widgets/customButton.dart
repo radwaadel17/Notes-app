999e5d5c03d8ff41f552bc478e790a771cabb1ba
radwaadel17 <radwaadel959@gmail.com>
2024-11-19 19:07:57 +0200

    add hive , hive flutter , build runner and hive generator

--- app/lib/widgets/customButton.dart
+++ app/lib/widgets/customButton.dart
@@ -7,14 +7,17 @@ class customButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    width: MediaQuery.of(context).size.width,
-    height: 40.h,
-    decoration: BoxDecoration(
-      color: const Color(0xff62FCD7),
-      borderRadius: BorderRadius.circular(16.r),
-    ),
-    child:  Center(child: Text('Add' , style: TextStyle(color: Colors.black , fontSize: 18.sp , fontWeight: FontWeight.w800),)),
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+      width: MediaQuery.of(context).size.width,
+      height: 40.h,
+      decoration: BoxDecoration(
+        color: const Color(0xff62FCD7),
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      child:  Center(child: Text('Add' , style: TextStyle(color: Colors.black , fontSize: 18.sp , fontWeight: FontWeight.w800),)),
+      ),
     );
   }
 }
